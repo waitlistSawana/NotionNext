261326a2ced8ea85d4e238b7766b8814a7cb08c1
SawanaHSN <130306226+waitlistSawana@users.noreply.github.com>
2024-02-22 22:45:58 +0800

    taggroups

--- themes/hsawana-heo/index.js
+++ themes/hsawana-heo/index.js
@@ -156,6 +156,9 @@ const LayoutPostList = props => {
       <div id="post-outer-wrapper" className="px-5  md:px-0">
         {/* 文章分类条 */}
         <CategoryBar {...props} />
+        <div className="mb-2">
+          <TagGroups tags={props.tagOptions} currentTag={props.tag} />
+        </div>
         {siteConfig('POST_LIST_STYLE') === 'page'
           ? (
             <BlogPostListPage {...props} />
